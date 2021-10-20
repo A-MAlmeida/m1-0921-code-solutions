7f7999da8afd4113ee191625c2b2827266570053
Andrew <mister_andrew1@yahoo.com>
2021-10-19 20:08:02 -0700

    Completed javascript-arrays exercise

---
+++ javascript-arrays/main.js
@@ -0,0 +1,27 @@
+var colors = ['red', 'white', 'blue'];
+
+console.log('first value of colors[0]: ', colors[0]);
+console.log('second value of colors[1]: ', colors[1]);
+console.log('third value of colors[2]: ', colors[2]);
+
+console.log('America is', colors[0] + ', ' + colors[1] + ', ' + 'and ' + colors[2] + '.');
+
+colors[2] = 'green';
+
+console.log('Mexico is', colors[0] + ', ' + colors[1] + ', ' + 'and ' + colors[2] + '.');
+
+console.log('Value of colors array: ', colors);
+
+var students = ['AJ', 'Anthony', 'Andrew', 'Alex'];
+
+var numberOfStudents = students.length;
+
+console.log('There are ' + numberOfStudents + ' students' + ' in the class.');
+
+var lastIndex = numberOfStudents - 1;
+
+var lastStudent = students[lastIndex];
+
+console.log('The last student in the array is ' + lastStudent + '.');
+
+console.log('Value of students array: ', students);
