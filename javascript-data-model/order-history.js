1f0859e7993cb2f15251df937890805eedb39bd8
Andrew <mister_andrew1@yahoo.com>
2021-10-25 16:05:21 -0700

    Completed javscript-data-model exercie

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,51 @@
+var orderHistory = [
+
+  {
+    orderDate: 'August 4, 2020',
+    orderTotal: '$34.00',
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    deliveryDate: 'Aug 8, 2020',
+    item: 'Javascript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    returnWindow: 'Closed on Sep 7, 2020',
+    refundAmount: '$31.55'
+  },
+
+  {
+    orderDate: 'July 19, 2020',
+    orderTotal: '$44.53',
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    deliveryDate: 'Jul 20, 2020',
+    item: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    returnWindow: 'Closed on Aug 19, 2020',
+    refundAmount: '$41.33'
+  },
+
+  {
+    orderDate: 'July 4, 2020',
+    orderTotal: '$17.22',
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    deliveryDate: 'Jul 7, 2020',
+    item: 'GameCube Controller Adapter',
+    author: '',
+    returnWindow: 'Closed on Aug 5, 2020',
+    refundAmount: '15.98'
+  },
+
+  {
+    orderDate: 'July 3, 2020',
+    orderTotal: '$138.93',
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    deliveryDate: 'Jul 5, 2020',
+    item: 'GameCube Controller, The Art of Sql',
+    author: ' , Faroult, Stephen',
+    returnWindow: 'Closed on Aug 4, 2020',
+    refundAmount: '$94.95, $33.99'
+  }
+
+];
