b54c5307495c8fd3a4b7a08e47a2301fee3d0522
Andrew <mister_andrew1@yahoo.com>
2021-11-15 13:23:13 -0800

    Completed dom-manpipulation exercise

---
+++ dom-manipulation/main.js
@@ -0,0 +1,30 @@
+var clickCount = 0;
+
+var $hotButton = document.querySelector('.hot-button');
+
+var $clickCount = document.querySelector('.click-count');
+
+$hotButton.addEventListener('click', function () {
+
+  clickCount++;
+  var temperature;
+
+  if (clickCount < 4) {
+    temperature = 'cold';
+  } else if (clickCount < 7) {
+    temperature = 'warm';
+  } else if (clickCount < 10) {
+    temperature = 'tepid';
+  } else if (clickCount < 13) {
+    temperature = 'warm';
+  } else if (clickCount < 16) {
+    temperature = 'hot';
+  } else {
+    temperature = 'nuclear';
+  }
+
+  $hotButton.className = 'hot-button ' + temperature;
+
+  $clickCount.textContent = 'Clicks: ' + clickCount;
+
+});
