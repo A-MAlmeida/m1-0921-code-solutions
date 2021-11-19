be2caac1721aa6e1c4dd6988868274b4f738ae3e
Andrew <mister_andrew1@yahoo.com>
2021-11-18 21:56:18 -0800

    saving work

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,25 @@
+/*
+
+var $container = document.querySelector('.tab-container');
+
+var $tab = document.querySelector('.tab');
+
+var $tabNodes = $tab.childNodes;
+
+console.log($tabNodes);
+
+var $view = document.querySelector('.view');
+
+var $viewNodes = $view.childNodes;
+
+console.log($viewNodes);
+
+$container.addEventListener('click', function (event) {
+  if (event.target.tagName === 'tab') {
+    for(var i = 0; i < $tab.Nodes.length; i++) {
+
+    }
+  }
+});
+
+*/
